a826c1c8230456e7dbe888120b546ecc46d5aaf9
nakatake <tonarinototorototoro8972@gmail.com>
2024-06-12 04:28:48 +0900

    add mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3010;
+const uri = process.env.MONGODB_CONNECTION_STRING;
+
+// DB接続
+try{
+  if(!uri) throw new Error("DB接続エラー: URIが設定されていません");
+  await mongoose.connect(uri);
+  } catch (error) {
+  console.log(uri)
+  console.log("DB接続エラー", error);
+}
 
 app.get("/", (_, res) => {
   return res.send("Hello World!!");
